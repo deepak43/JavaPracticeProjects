e04f55645f8ae0e2f78e5ea68f21f7cee2788ebc
vaidyade <deepak.bal.vaidya@hpe.com>
2019-01-05 16:55:35 +0530

    first commit

---
+++ JDBCTest/src/Test/JdbcTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JdbcTest {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/deepak", "root", "admin");
+		System.out.println(con);
+		
+		
+		//DDL, DML, DRL
+		
+		con.close();
+	}
+
+}
